be3cf8638759d20498d525103e489cb186ec4948
Eugen Ryabinin <eugene.ryabinin@devexpress.com>
2019-12-20 13:01:41 +0300

    Updates

---
+++ precache-manifest.3f24aa98853f2664e3e4fa7a814b193d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93dc1c05266dcce852474f3f403363a",
+    "url": "/TestReact/index.html"
+  },
+  {
+    "revision": "1e01cae0ebaecb7e185b",
+    "url": "/TestReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5debdd75d471bb9d7946",
+    "url": "/TestReact/static/js/2.824c0115.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TestReact/static/js/2.824c0115.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e01cae0ebaecb7e185b",
+    "url": "/TestReact/static/js/main.2c48391f.chunk.js"
+  },
+  {
+    "revision": "4c4c95d1ca80e4f2252b",
+    "url": "/TestReact/static/js/runtime-main.89bc2724.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
